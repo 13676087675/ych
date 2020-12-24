d60f4561967141b841080eca9e250f8b7b892580
ych <1961857865@qq.com>
2020-12-24 16:27:15 +0800

    修改字符集为utf8

--- src/test/java/com/ych/B.java
+++ src/test/java/com/ych/B.java
@@ -9,13 +9,13 @@ import java.lang.management.MemoryUsage;
 public class B{
         MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
 
-        MemoryUsage memoryUsage = memoryMXBean.getHeapMemoryUsage(); //椎内存使用情况
+        MemoryUsage memoryUsage = memoryMXBean.getHeapMemoryUsage();
 
-        long totalMemorySize = memoryUsage.getInit(); //初始的总内存
+        long totalMemorySize = memoryUsage.getInit();
 
-        long maxMemorySize = memoryUsage.getMax(); //最大可用内存
+        long maxMemorySize = memoryUsage.getMax();
 
-        long usedMemorySize = memoryUsage.getUsed(); //已使用的内存
+        long usedMemorySize = memoryUsage.getUsed();
 
        @Test
        public void test(){
